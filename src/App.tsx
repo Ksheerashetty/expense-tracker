21a34d278d3049dd3ee8efec887dc5a52097c802
Ksheera Shetty <ksheerashetty@gmail.com>
2025-07-20 22:44:29 +0530

    introduction of useState

--- src/App.tsx
+++ src/App.tsx
@@ -11,51 +11,20 @@ type Transaction = {
   amount: number;
   description: string;
 };
+
 function App() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-  // const transactions = [
-  //   {
-  //     id: 1,
-  //     date: "2025-07-01",
-  //     category: "Food",
-  //     amount: 20.0,
-  //     description: "Groceries",
-  //   },
-  //   {
-  //     id: 2,
-  //     date: "2025-07-02",
-  //     category: "Transport",
-  //     amount: 15.0,
-  //     description: "Bus Ticket",
-  //   },
-  //   {
-  //     id: 3,
-  //     date: "2025-07-03",
-  //     category: "Utilities",
-  //     amount: 100.0,
-  //     description: "Electricity Bill",
-  //   },
-  //   {
-  //     id: 4,
-  //     date: "2025-07-04",
-  //     category: "Utilities",
-  //     amount: 50.0,
-  //     description: "Water Bill",
-  //   },
-  // ];
-  //const transactions = [200, 150, 300];
-
-  const total = transactions.reduce(
-    (sum, transactions) => sum + transactions.amount,
-    0
+  const categories = transactions.reduce<{ [key: string]: number }>(
+    (cat, tx) => {
+      cat[tx.category] = (cat[tx.category] || 0) + tx.amount;
+      return cat;
+    },
+    {}
   );
-
-  const categories: { [key: string]: number } = {};
-  transactions.forEach((tx) => {
-    categories[tx.category] = (categories[tx.category] || 0) + tx.amount;
-  });
+  
 
   const data = {
     labels: Object.keys(categories),
@@ -74,64 +43,130 @@ function App() {
 
   const transactionList = transactions.map((ts) => (
     <li key={ts.id}>
-      {ts.date} - {ts.category} - ₹{ts.amount.toFixed(2)}
+      {ts.date} - {ts.category} - {ts.description} -{" "}
+      {ts.amount.toLocaleString("en-IN", {
+        style: "currency",
+        currency: "INR",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })}
     </li>
   ));
 
-  // function handleClick(event: {
-  //   preventDefault: any;
-  //   currentTarget: HTMLFormElement | undefined;
-  // }) {
-  //   event.preventDefault();
-  //   const formData = new FormData(event.currentTarget);
-  //   const newExpense = formData.get("expense");
-  //   const parsed = newExpense ? parseFloat(newExpense as string) : 0;
-  //   const newTransaction: Transaction = {
-  //   id: Date.now(), // generates a unique ID
-  //   date: new Date().toISOString().split("T")[0], // today's date
-  //   category: "Misc", // default category
-  //   amount: parsed,
-  //   description: "Added manually", // default description
-  // };
+  const total = transactions
+    .reduce((sum, transactions) => sum + transactions.amount, 0)
+    .toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
 
-  // setTransactions((prev) => [...prev, newTransaction]);
-  //   // setTransactions([...transactions, parsed]);
-  // }
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    //const date = (form.elements.namedItem("date") as HTMLInputElement).value;
+    const newExpense: Transaction = {
+      id: Date.now(),
+      date: new Date(
+        (form.elements.namedItem("date") as HTMLInputElement).value
+      ).toLocaleDateString(),
+      category:
+        (form.elements.namedItem("category") as HTMLInputElement).value
+          .charAt(0)
+          .toUpperCase() +
+        (form.elements.namedItem("category") as HTMLInputElement).value
+          .slice(1)
+          .toLocaleLowerCase(),
+      amount: parseFloat(
+        (form.elements.namedItem("amount") as HTMLInputElement).value
+      ),
+      description: (form.elements.namedItem("description") as HTMLInputElement)
+        .value,
+    };
+    // const formattedDate = new Date(date).toLocaleDateString();
+    if (
+      newExpense.amount <= 0 ||
+      isNaN(newExpense.amount) ||
+      !newExpense.description ||
+      !newExpense.category ||
+      !newExpense.date
+    ) {
+      setError("Please enter valid details");
+      return;
+    }
+    setError(null);
+
+    setTransactions((prev) => [...prev, newExpense]);
+    setIsOpenModal((prev) => !prev);
+  }
 
   return (
     <main>
       <h1>Expense Tracker</h1>
       <h3>Total spent</h3>
-      <form>
-        <p className="total">₹{total.toFixed(2)}</p>
-        {/* <input type="text" placeholder="Add new expense" /> */}
-        <br />
-        <button
-          onClick={(e) => {
-            setIsOpenModal(true);
-            e.preventDefault();
-          }}
-        >
-          Add Expense
-        </button>
-      </form>
-      {isOpenModal && (
-        <div className="overlay">
-          <div className="modal">
-            <input type="text" placeholder="Date (YYYY-MM-DD)"></input>
-            <input type="text" placeholder="Category"></input>
-            <input type="number" placeholder="Amount"></input>
-            <input type="text" placeholder="Description"></input>
+      <p className="total">{total}</p>
+      <br />
+      <button
+        onClick={(e) => {
+          setIsOpenModal(true);
+          e.preventDefault();
+        }}
+      >
+        Add Expense
+      </button>
 
-            <div className="button-group">
-              <button>Submit</button>
-              <button onClick={() => setIsOpenModal(false)}>Close</button>
+      {isOpenModal && (
+        <form onSubmit={handleSubmit}>
+          <div className="overlay">
+            <div className="modal">
+              {error && (
+                <div className="alert-info" role="alert">
+                  {error}
+                </div>
+              )}
+              <input
+                type="date"
+                placeholder="Date (MM-DD-YYYY)"
+                name="date"
+                required
+              ></input>
+              <input
+                type="text"
+                placeholder="Category"
+                name="category"
+                required
+              ></input>
+              <input
+                type="number"
+                placeholder="Amount"
+                name="amount"
+                required
+              ></input>
+              <input
+                type="text"
+                placeholder="Description"
+                name="description"
+                required
+              ></input>
+              <div className="button-group">
+                <button type="submit">Submit</button>
+
+                <button type="button" onClick={() => setIsOpenModal(false)}>
+                  Close
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       )}
-      <p>Recent transactions</p>
-      <ul>{transactionList}</ul>
+      <p className="recent-transactions">Recent transactions</p>
+      {transactions.length === 0 ? (
+        "No transactions yet"
+      ) : (
+        <ul>{transactionList}</ul>
+      )}
+
       <Doughnut
         className="chart"
         data={data}
